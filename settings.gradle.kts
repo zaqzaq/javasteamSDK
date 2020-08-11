b254a96c090e6dec7175b129f06844b728cf12c4
Karl Sabo <karl@nimblygames.com>
2020-08-11 08:25:07 -0600

    Update Gradle plugin coordinates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -49,7 +49,7 @@ pluginManagement {
    resolutionStrategy {
       eachPlugin {
          if (requested.id.id == "com.nimblygames.gradle") {
-            useModule("com.nimblygames:GradlePlugin:1.0.8-SNAPSHOT")
+            useModule("com.nimblygames.gradle:GradlePlugin:1.0.8-SNAPSHOT")
          }
       }
    }
